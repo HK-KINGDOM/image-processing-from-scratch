cd1f31e075136599795fb968d0794fc05b11f975
lixin <2270698884@qq.com>
2021-07-14 12:03:53 -0700

    add c++ implementation for SIFT

---
+++ FeaturePointsCollections/SIFT/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by lixin on 2021/6/19.
+//
+#include "SIFT.h"
+int main()
+{
+    cv::Mat img = cv::imread("./images/6.jpg");
+    cv::imshow("1",img);
+    cv::waitKey(0);
+
+    cv::Mat img_ = cv::imread("./images/06.jpg");
+    cv::imshow("1",img_);
+    cv::waitKey(0);
+
+    SIFT::Keypoints keypoints = SIFT::extract(img);
+    SIFT::Keypoints keypoints_ = SIFT::extract(img_);
+
+    SIFT::Keypoints m1,m2;
+    SIFT::match(keypoints,keypoints_,m1,m2);
+    cv::Mat res = SIFT::linkPoints(img,m1,img_,m2);
+    cv::imshow("1",res);
+    cv::waitKey(0);
+
+    return 0;
+}
